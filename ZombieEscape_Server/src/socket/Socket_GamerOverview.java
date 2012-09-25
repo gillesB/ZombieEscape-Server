dd916afca6bd489793c7d31ddad0182a75de2c29
gillesB <baatzgilles@googlemail.com>
2012-09-25 12:28:14 +0200

    renamed boolean zombie to isZombie

--- ZombieEscape_Server/src/socket/Socket_GamerOverview.java
+++ ZombieEscape_Server/src/socket/Socket_GamerOverview.java
@@ -3,7 +3,7 @@ package socket;
 public class Socket_GamerOverview {
 
 	public String gamername;
-	public boolean zombie;
+	public boolean isZombie;
 	public double longitude;
 	public double latitude;
 }
