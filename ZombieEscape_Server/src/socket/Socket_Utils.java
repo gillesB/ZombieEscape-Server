43fa7dda3030a9ae2559923cb5d8f65f280bdf59
gillesB <baatzgilles@googlemail.com>
2012-09-28 16:43:37 +0200

    transformGamerslistToSocket_GamerOverviewList() argument can be a Collection

--- ZombieEscape_Server/src/socket/Socket_Utils.java
+++ ZombieEscape_Server/src/socket/Socket_Utils.java
@@ -1,6 +1,7 @@
 package socket;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import server.GPS_location;
 import server.Gamer;
@@ -8,9 +9,9 @@ import server.Gamer;
 public class Socket_Utils {
 
 	
-	public static ArrayList<Socket_GamerOverview> transformGamerslistToSocket_GamerOverviewList(ArrayList<Gamer> gamers){
-		ArrayList<Socket_GamerOverview> overview = new ArrayList<Socket_GamerOverview>(gamers.size());
-		for (Gamer g : gamers) {
+	public static ArrayList<Socket_GamerOverview> transformGamerslistToSocket_GamerOverviewList(Collection<Gamer> collection){
+		ArrayList<Socket_GamerOverview> overview = new ArrayList<Socket_GamerOverview>(collection.size());
+		for (Gamer g : collection) {
 			Socket_GamerOverview s = new Socket_GamerOverview();
 			s.gamername = g.getName();
 			GPS_location gps = g.getLocation();
