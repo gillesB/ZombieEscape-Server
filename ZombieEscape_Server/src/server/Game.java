4e49ad4ed63419c7266f1491d4a31677b3304e8a
gillesB <baatzgilles@gmailcom>
2012-09-30 12:13:15 +0200

    found an error in zombiecount and humancount

--- ZombieEscape_Server/src/server/Game.java
+++ ZombieEscape_Server/src/server/Game.java
@@ -50,10 +50,13 @@ public class Game implements Runnable {
 		synchronized (gamer) {
 			if (state == 1) { // gamer becomes human
 				gamer.setZombie(false);
+				zombieCount.getAndIncrement();
 			} else if (state == 2) { // gamer becomes zombie
 				gamer.setZombie(true);
+				humanCount.getAndIncrement();
 			} else {
 				// does the gamer become a zombie or a human?
+				System.out.println("zombieCount.get() < humanCount.get() " + zombieCount.get() + " < " + humanCount.get());
 				if (zombieCount.get() < humanCount.get()) {
 					gamer.setZombie(true);
 					zombieCount.getAndIncrement();
